2b0dc9bb7d59099849aabdd637c54ecafd617eb7
Scott <scrosehart@gmail.com>
2018-04-09 13:03:03 -0700

    added footer and deleted templates dir

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import Nav from './components/Nav'
+import Footer from './components/Footer'
 import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.component('Nav', Nav);
+Vue.component('Footer', Footer);
 
 /* eslint-disable no-new */
 new Vue({
