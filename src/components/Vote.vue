a790fdd623e3cb029cb7432343036a0b3bd45d06
Sean Cesmat <sean.cesmat@gmail.com>
2018-04-09 16:00:00 -0700

    added vote button, cleaned up feedsnap

--- src/components/Vote.vue
+++ src/components/Vote.vue
@@ -1,5 +1,14 @@
 <template>
-  <button>Vote Button</button>
+  <div>
+    <a href="#">
+      <div class="up-icon">
+        <i class="fas fa-caret-circle-up"></i>
+      </div>
+      <div class="vote-num">150</div>
+    </a>
+  </div>
+
+
 </template>
 
 <script>
@@ -15,5 +24,23 @@ export default {
 <style scoped>
   h2 {
     color: red
+  }
+  a {
+    display: flex;
+    align-items: center;
+  }
+  .up-icon {
+    font-size: 2rem;
+    z-index: 2;
+  }
+  i {
+    background: white;
+    border-radius: 100%;
+  }
+  .vote-num {
+    background: #333;
+    border-radius: 2px;
+    padding: 0px 10px 0px 20px;
+    margin-left: -10px;
   }
 </style>
