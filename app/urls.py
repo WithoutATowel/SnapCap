f8e3d91b626b81904ca16917a3d0989484f2327a
Brett <brettspencr@gmail.com>
2018-04-09 21:39:39 -0700

    Added signup serializer + view

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
+router.register('users', views.UserView)
 router.register('profile', views.ProfileView)
 router.register('snaps', views.PictureView)
 router.register('caps', views.UsercapView)
@@ -14,14 +15,15 @@ router.register('friends', views.FriendshipView)
 urlpatterns = [
     path('', views.index, name='index'),
     url('api/snaps/(?P<category>\D+)/$', views.PictureView.as_view({'get': 'list'})),
+    url('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    # path('snaps/friends/', views.friends, name='friends'),
-    # path('snaps/<str:category>/', views.category, name='category'),
-    # path('snaps/<int:snap_id>/vote/', views.snap_vote, name='snap_vote'),
     # path('signup/', views.signup, name='signup'),
     # path('login/', views.login, name='login'),
     # path('logout/', views.logout, name='logout'),
+    
+    # path('snaps/friends/', views.friends, name='friends'),
+    # path('snaps/<int:snap_id>/vote/', views.snap_vote, name='snap_vote'),
+    
     # path('profile/<int:profile_id>/', views.profile, name='profile'),
     # path('caps/<int:cap_id>/<int:is_card>/', views.cap_vote, name='cap_vote'),
 ]
@@ -30,3 +32,4 @@ urlpatterns = [
 # COVERED BY THE REST FRAMEWORK ROUTER
 # path('snaps/', views.snaps, name='snaps'),
 # path('snaps/<int:snap_id>/', views.details, name='details'),
+# path('snaps/<str:category>/', views.category, name='category'),
