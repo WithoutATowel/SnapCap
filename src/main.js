f678b390db075b4ef37bfc36630e3f3d73a4b341
Brett <brettspencr@gmail.com>
2018-04-12 21:05:46 -0700

    Fixed auth?

--- src/main.js
+++ src/main.js
@@ -20,8 +20,8 @@ const store = new Vuex.Store({
     jwt: localStorage.getItem('t'),
     user: localStorage.getItem('u'),
     endpoints: {
-      obtainJWT: 'http://localhost:8000/api/auth/obtain_token/',
-      refreshJWT: 'http://localhost:8000/api/auth/refresh_token/'
+      obtainJWT: 'https://snapcap-app.herokuapp.com/auth/obtain_token/',
+      refreshJWT: 'https://snapcap-app.herokuapp.com/auth/refresh_token/'
     }
   },
   mutations: {
